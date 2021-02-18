640a72d0ef70de1b724b32b75ff12be6a0aa66cb
Abibou <abibou2017@gmail.com>
2021-02-18 15:41:23 -0600

    tagged the navigation bar on every page, fixed the local storage variable

--- src/components/FuelQuoteForm.js
+++ src/components/FuelQuoteForm.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from "./NavBar";
 
 
 const FuelQuoteForm = () => {
     return (
         <div>
+            <NavBar/>
             <p className="text-success text-center">I am the Fuel Quote Form</p>
         </div>
     )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,12 +16,12 @@ const NavBar = () => {
     return (
         <nav style={styleNavBar} className="navbar navbar-expand-lg navbar-light justify-content-between p-4">
             <div>
-                <a href="/clientprofile" style={styleLink} className="navbar-brand bg-light rounded p-1">Home</a>
-                <a href="/fuelquote" style={styleLink} className="navbar-brand bg-light rounded p-1">Fuel Form</a>
+                <a href="/clientprofile" style={styleLink} className="navbar-brand ml-5 bg-light rounded p-1">Home</a>
+                <a href="/fuelquote" style={styleLink} className="navbar-brand ml-4 bg-light rounded p-1">Fuel Form</a>
             </div>
             <div>
                 <small style={styleLink} className="navbar-brand border border-white rounded p-1">Welcome!</small>
-                <a href="/" style={styleLink} className="navbar-brand bg-light rounded p-1 ml-4 text-dark">Sign Out</a>
+                <a href="/" style={styleLink} className="navbar-brand bg-light rounded p-1 ml-4 mr-5 text-dark">Sign Out</a>
             </div>
         </nav>
     );
