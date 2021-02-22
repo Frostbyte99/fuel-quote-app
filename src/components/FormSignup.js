a5a9d9682bb52a4faf203d420cda8618914ce900
frostbyte99 <frostbyte498@gmail.com>
2021-02-22 14:10:14 -0600

    Formatting

--- src/components/FormSignup.js
+++ src/components/FormSignup.js
@@ -1,23 +1,19 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
+import "./FormSignup.css";
 
 const FormSignup = (props) => {
-
   const divStyle = {
-    position: 'absolute',
-    left: '50%',
-    top: '50%',
-    transform: 'translate(-50%, -50%)',
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    transform: "translate(-50%, -50%)",
   };
-
-  const [errors, setErrors] = useState({});
 
   const [values, setValues] = useState({
     username: "",
-    email: "",
     password: "",
-    password2: "",
   });
 
   const handleChange = (e) => {
@@ -32,66 +28,60 @@ const FormSignup = (props) => {
     console.log("Form submitted!");
     console.log(values);
     e.preventDefault();
-    
-    
 
     // TODO: save account information
 
     // Redirect to login page to complete account setup
-    props.history.push('/');
-
+    props.history.push("/");
   };
 
   return (
-    <div style={divStyle} className="text-center flex-direction mt-0 p-4 w-25 flex-column container bg-light border rounded">
+    <div
+      style={divStyle}
+      className="text-center flex-direction mt-0 p-4 w-25 flex-column container bg-light border rounded"
+    >
       <h3>Please fill out the information below to make an account</h3>
-      <form className="border-0 d-flex align-items flex-column mx-auto w-100" onSubmit={handleSubmit}>
-          <label htmlFor="username" className="text-left mb-0">
-            Username
-          </label>
-          <input className="w-100 mb-0"
-            id="username"
-            type="text"
-            name="username"
-            placeholder="Enter your username"
-            value={values.username}
-            onChange={handleChange}
-            required
-            pattern="[A-Za-z0-9]+"
-            title="Only letters and numbers"
-          />
-          <label htmlFor="password" className="text-left mb-0">
-            Password
-          </label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-            value={values.password}
-            onChange={handleChange}
-            required
-            pattern="[A-Za-z0-9]+"
-            title="Only letters and numbers"
-          />
-          <label htmlFor="password2" className="text-left mb-0">
-            Confirm your password
-          </label>
-          <input
-            id="password2"
-            type="password"
-            name="password2"
-            placeholder="Re-Enter your password"
-            value={values.password2}
-            onChange={handleChange}
-            required
-            pattern="[A-Za-z0-9]+"
-            title="Only letters and numbers"
-          />
-        <input className="bg-primary border-primary text-white mt-2" type="submit" value="Signup"/>
+      <form
+        className="border-0 d-flex align-items flex-column mx-auto w-100"
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="username" className="text-left mb-0">
+          Username
+        </label>
+        <input
+          className="w-100 mb-0"
+          id="username"
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={values.username}
+          onChange={handleChange}
+          required
+          pattern="[A-Za-z0-9]+"
+          title="Only letters and numbers"
+        />
+        <label htmlFor="password" className="text-left mb-0">
+          Password
+        </label>
+        <input
+          id="password"
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={values.password}
+          onChange={handleChange}
+          required
+          pattern="[A-Za-z0-9]+"
+          title="Only letters and numbers"
+        />
+        <input
+          className="bg-primary border-primary text-white mt-2"
+          type="submit"
+          value="Signup"
+        />
       </form>
       <small>
-      Already have an account? Log in
+        Already have an account? Log in
         <Link to="/"> here</Link>
       </small>
     </div>
