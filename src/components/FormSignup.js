0f5a3b305df74ddfc625f3788052343d66f6cda2
frostbyte99 <frostbyte498@gmail.com>
2021-02-24 12:58:53 -0600

    Minor edits

--- src/components/FormSignup.js
+++ src/components/FormSignup.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
-import "./FormSignup.css";
 
 const FormSignup = (props) => {
   const divStyle = {
