21030a9452c1daa0a0ed92ea8dbe38924d3952ec
frostbyte99 <frostbyte498@gmail.com>
2021-03-14 20:37:29 -0500

    added basic model

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
 # created temporary model for testing
 
-class TestModel(models.Model):
-    name = models.CharField(max_length=200)
-    status = models.BooleanField(default=False, blank=True, null=True)
+# Create your models here.
 
-    def __str__(self):
-        return self.name
+class Task(models.Model):
+  title = models.CharField(max_length=200)
+  completed = models.BooleanField(default=False, blank=True, null=True)
+      
+  def __str__(self):
+    return self.title
