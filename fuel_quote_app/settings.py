4820915de2f3be58c6612de48315fea5bb96c535
frostbyte99 <frostbyte498@gmail.com>
2021-03-16 17:08:25 -0500

    updated settings

--- fuel_quote_app/settings.py
+++ fuel_quote_app/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend.apps.BackendConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'fuel_quote_app.urls'
@@ -126,4 +128,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build/static')
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
 ]
