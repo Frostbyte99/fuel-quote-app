30a3743fcf518bc7f544ae102430378f9d345b8e
Abibou <abibou2017@gmail.com>
2021-03-19 15:00:43 -0500

    created login module

---
+++ backend/login_module.py
@@ -0,0 +1,21 @@
+def validateUserName(userName):
+    if type(userName) != str:
+        return False
+    if not userName: #empty string
+        return False
+    if len(userName) < 5: # username must be greater than 4 in length
+        return False
+    if not all(i.isalpha() or i.isnumeric for i in userName): # userName must be alphanumeric only
+        return False
+    return True
+
+def validatePassword(password):
+    if type(Fassword) != str:
+        return False
+    if not password: #empty string
+        return False
+    if not all(i.isspace() for i in password): # can't contain space
+        return False
+    if len(password) < 6: #password must be at least 6 character
+        return False
+    return True
