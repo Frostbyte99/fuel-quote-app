8162bb1d15ac51ea7c4c673395715aa109bb6fa4
Bryce <frostbyte498@gmail.com>
2021-03-19 14:26:19 -0500

    fixed inconsistant tabs

--- backend/serializers.py
+++ backend/serializers.py
@@ -2,19 +2,19 @@ from rest_framework import serializers
 from .models import Profile, Quote, User
 
 class UserSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = User
-		fields = '__all__'
+    class Meta:
+        model = User
+        fields = '__all__'
 
 class ProfileSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Profile
-		fields = ('userID', 'fullName', 'address1', 'address2', 'city', 'usState', 'zipcode')
+    class Meta:
+        model = Profile
+        fields = ('userID', 'fullName', 'address1', 'address2', 'city', 'usState', 'zipcode')
 
 class QuoteSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Quote
-    fields = ('userID', 'gallons', 'address', 'deliveryDate', 'totalPrice',)
+    class Meta:
+        model = Quote
+        fields = ('userID', 'gallons', 'address', 'deliveryDate', 'totalPrice')
 
 class LoginSerializerWithToken(serializers.ModelSerializer):
 
