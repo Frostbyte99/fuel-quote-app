f2e22c88bfefb6708e55c3412cf414cec0c66158
Bryce <frostbyte498@gmail.com>
2021-03-31 20:38:47 -0500

    Changed names of models, added F keys

--- backend/models.py
+++ backend/models.py
@@ -1,18 +1,11 @@
-import uuid
 from django.db import models
 
-class User(models.Model):
+class UserCredentials(models.Model):
   userName = models.CharField(max_length=100, unique=True)
-  userID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
   password = models.CharField(max_length=200)
-  is_active = models.BooleanField(default=True)
-  # foreignKey to the Profile of this User
 
-# from django.contrib.auth.models import User
-# class UserProfile(models.Model):
-#     user = models.OneToOneField(User)
-class Profile(models.Model):
-  userID = models.UUIDField(blank=True, null=True)
+class ClientInformation(models.Model):
+  UserCredentials = models.ForeignKey(UserCredentials, on_delete=models.CASCADE)
   fullName = models.CharField(max_length=50, default="Example Name")
   address1 = models.CharField(max_length=100, default="Example Address 1")
   address2 = models.CharField(max_length=100, default="Example Address 2")
@@ -20,10 +13,9 @@ class Profile(models.Model):
   usState = models.CharField(max_length=2, default="Example State")
   zipcode = models.CharField(max_length=9, default="Example Zip")
 
-class Quote(models.Model):
-  userID = models.UUIDField(blank=True, null=True)
+class FuelQuote(models.Model):
+  UserCredentials = models.ForeignKey(UserCredentials, on_delete=models.CASCADE)
   gallons = models.IntegerField(default=0)
   address = models.CharField(max_length=100, blank=True, null=True, default="Example Address")
   deliveryDate = models.CharField(max_length=100, blank=True, null=True, default="Example deliveryDate")
   totalPrice = models.IntegerField(default=0)
-  # foreignKey to Profile of Client who submitted this Quote
