1209fad36aad854412c04b273c6c1090a37087d6
Bryce <frostbyte498@gmail.com>
2021-04-01 22:24:28 -0500

    fixed models

--- backend/models.py
+++ backend/models.py
@@ -5,16 +5,16 @@ class UserCredentials(models.Model):
   password = models.CharField(max_length=200)
 
 class ClientInformation(models.Model):
-  UserCredentials = models.ForeignKey(UserCredentials, on_delete=models.CASCADE)
+  userName = models.CharField(max_length=100, blank=True, null=True)
   fullName = models.CharField(max_length=50, default="Example Name")
   address1 = models.CharField(max_length=100, default="Example Address 1")
   address2 = models.CharField(max_length=100, default="Example Address 2")
   city = models.CharField(max_length=100, default="Example City")
-  usState = models.CharField(max_length=2, default="Example State")
-  zipcode = models.CharField(max_length=9, default="Example Zip")
+  usState = models.CharField(max_length=2, default="TX")
+  zipcode = models.CharField(max_length=9, default="22222")
 
 class FuelQuote(models.Model):
-  UserCredentials = models.ForeignKey(UserCredentials, on_delete=models.CASCADE)
+  userName = models.CharField(max_length=100, blank=True, null=True)
   gallons = models.IntegerField(default=0)
   address = models.CharField(max_length=100, blank=True, null=True, default="Example Address")
   deliveryDate = models.CharField(max_length=100, blank=True, null=True, default="Example deliveryDate")
