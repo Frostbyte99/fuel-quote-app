24150df7f333036413cfc3f986b7e362800fce15
Bryce <frostbyte498@gmail.com>
2021-04-22 20:50:56 -0500

    added pricing module

---
+++ backend/pricing_module.py
@@ -0,0 +1,37 @@
+from .serializers import ProfileSerializer, QuoteSerializer, UserSerializer
+from .models import ClientInformation, FuelQuote, UserCredentials
+
+
+def calculateTotalPrice(profileName, gallons):
+
+    gallons = int(gallons)
+
+    currentPrice = 1.50
+    locationFactor = 0.0
+    rateHistoryFactor = 0.0
+    gallonsRequestedFactor = 0.0
+    companyProfitFactor = 0.1
+
+    clientInfo = ClientInformation.objects.filter(userName=profileName).order_by('id').first()
+    infoSerializer = ProfileSerializer(clientInfo, many=False)
+
+    if infoSerializer.data['usState'] == 'TX':
+        locationFactor = 0.02
+    else:
+        locationFactor = 0.04
+
+    if FuelQuote.objects.filter(userName=profileName):
+        rateHistoryFactor = 0.01
+    else:
+        rateHistoryFactor = 0.00
+
+    if gallons > 1000:
+        gallonsRequestedFactor = 0.02
+    else:
+        gallonsRequestedFactor = 0.03
+
+    margin = currentPrice * (locationFactor - rateHistoryFactor + gallonsRequestedFactor + companyProfitFactor)
+    suggestedPrice = margin * currentPrice
+    totalPrice = suggestedPrice * gallons
+
+    return totalPrice
--- backend/urls.py
+++ backend/urls.py
@@ -16,13 +16,14 @@ urlpatterns = [
 	path('quote-create/', views.quoteCreate, name="quote-create"),
 	path('quote-update/<str:pk>/', views.quoteUpdate, name="quote-update"),
 	path('quote-delete/<str:pk>/', views.quoteDelete, name="quote-delete"),
+	path('quote-price/<str:name>/<str:gallons>/', views.getQuotePrice, name="quote-price"),
 	# Login
 	path('login/', views.login, name="login"),
   	path('signup/', views.signup, name="signup"),
 	# User API
 	path('user-list/', views.userList, name="user-list"),
 	path('user-create/', views.userCreate, name="user-create"),
-	path('user-getUserUUID/<str:pk>/', views.getUserUUID, name="user-getUserUUID"),
 	path('user-isUserUnique/<str:pk>/', views.isUserUnique, name="user-isUserUnique"),
 	path('user-getUserName/<str:pk>/', views.getUserName, name="user-getUserName"),
+	
 ]
