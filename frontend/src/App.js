1842841ac9650943f4c98b9c29f6e16e93e8b313
Daniel F <43554331+dferwave@users.noreply.github.com>
2021-04-23 23:18:58 -0500

    Updated to block URLs from unauthorized users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,42 @@
-  
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Route, useLocation, Switch } from 'react-router-dom';
 import Signup from './components/FormSignup';
 import Login from './components/LoginForm';
 import ClientProfile from './components/ClientProfileForm';
 import FuelQuote from './components/FuelQuoteForm';
 import FuelQuoteHistory from './components/FuelQuoteHistory';
-import axios from 'axios';
+import Axios from 'axios';
 
-const App = () => (
-    <Switch>
-        <Route path="/" exact component={Login} />
-        <Route path="/Signup" exact component={Signup} />
-        <Route path="/ClientProfile" exact component={ClientProfile} />
-        <Route path="/FuelQuote" exact component={FuelQuote} />
-        <Route path="/FuelQuoteHistory" exact component={FuelQuoteHistory} />
-    </Switch>
-);
+const App = () => {
+    const [isAuthenticated, userHasAuthenticated] = useState(false);
+    const location = useLocation();
+
+    useEffect(() => {
+        onLoad();
+    }, [location]);
+
+    async function onLoad() {
+        try {
+            if(sessionStorage.getItem('username')) {
+                userHasAuthenticated(true);
+            } else {
+                throw new ReferenceError("not logged in");
+            }
+            console.log("Current Path: '"+location.pathname+"'");
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    return (
+        <Switch>
+            <Route path="/" exact component={Login} />
+            <Route path="/Signup" exact component={Signup} />
+            <Route path="/ClientProfile" exact component={isAuthenticated ? ClientProfile : Login} />
+            <Route path="/FuelQuote" exact component={isAuthenticated ? FuelQuote : Login} />
+            <Route path="/FuelQuoteHistory" exact component={isAuthenticated ? FuelQuoteHistory : Login} />
+        </Switch>
+    );
+}
 
 export default App;
